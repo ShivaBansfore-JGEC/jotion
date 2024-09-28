74b8f9049819d707370c76a7d8a7316fe4a853da
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2024-09-28 18:47:47 +0530

    feat: some ui elements added and testimonial section added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import db from "../lib/superbase/db";
+import { ThemeProvide } from "@/lib/providers/next-theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvide
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+        >
+          {children}
+        </ThemeProvide>
       </body>
     </html>
   );
